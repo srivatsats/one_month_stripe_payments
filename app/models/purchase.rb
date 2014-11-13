8ee43a9071f17447aece8829e4ff51e0bab5ef28
Srivatsa Sreenivasarao <srivatsats@gmail.com>
2014-11-13 15:57:07 +0530

    live gateway

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,2 +1,29 @@
+require 'net/http'
+require 'open-uri'
+
 class Purchase < ActiveRecord::Base
+	cattr_reader :user
+
+	# before_create :authenticate_api_user
+	after_create :create_payment_info_in_easle
+
+
+	def authenticate_api_user
+		easle_api_url = URI("http://easledemo.smarton.co/api/sessions")
+		params = {}
+		params[:user] = { :email => "admin@smarton.co", :password => "5marton1" }
+		response = Net::HTTP.post_form(easle_api_url, params)
+		puts "api response -> #{response}"
+	end
+
+	def create_payment_info_in_easle
+		easle_api_url = URI("http://easledemo.smarton.co/website_payment_info")
+		puts "url -> #{easle_api_url}"
+		params =  { :course_name => self.product_id, :student_name => self.email, :email_id => self.email, :transaction_token => self.card,
+		 :amount => self.amount, :currency => self.currency, :gateway => "stripe", status: "success" }
+
+		response = Net::HTTP.post_form(easle_api_url, params) 
+		puts "api response -> #{response}"
+		return response
+	end
 end
