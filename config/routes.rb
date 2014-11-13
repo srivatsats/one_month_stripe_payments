749a75b9f88c128264e1ab86424f34616963486b
Srivatsa Sreenivasarao <srivatsats@gmail.com>
2014-11-13 11:51:32 +0530

    added ability to store purchases and display a receipt to customer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 OneMonthStripePayments::Application.routes.draw do
   root 'pages#home'
   resources :charges
+  resources :purchases, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
